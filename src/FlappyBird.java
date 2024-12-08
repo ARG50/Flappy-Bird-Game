0d6e39256e5a1b22d0695aee2adf7df979b7557d
Arghya Samanta <arghya.samanta50@gmail.com>
2024-12-08 10:41:23 +0530

    Game Concept introduced with collision method
    1. Bottom Pipes are set up below top pipe with passing space for the bird. Bottom pipes are added to array list for placing pipes in the frame as top pipe updates.
    2. Game over concept introduced with collision method and also if bird is fallen out of frame.
    3. If game over then stop the Timers for placings pipes in the frame and game loop which stops drawing graphics.

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -58,16 +58,21 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     Bird bird;
     // Movement in X direction(To show that pipes are passing at left side and bird is moving towards right side.)
     private int velocityX = -4;
+
     // Movement of bird in Y direction (Negative for Upwards, Positive for Downwards)
     private int velocityY = 0; // At the start, the bird is falling.
-    private int gravityFall = 1;
-    // Game Timer Loop --> 60 frames per second
-    Timer gameLoop;
-    // Timer for placing pipes.
-    Timer placePipesTimer;
+    private int gravityFall = 1; // Gravity fall
 
-    ArrayList<Pipe> pipesArrayList;
-    Random random = new Random();
+    // Timers
+    Timer gameLoop; // Game Timer Loop --> 60 frames per second
+    Timer placePipesTimer; // Timer for placing pipes.
+
+    // Is Game Over ?
+    boolean gameOver = false; // Check if collision occurs between the pipes or the bird is fallen out of the frame.
+
+    // Making a list of pipes with random position
+    ArrayList<Pipe> pipesArrayList; // list of holding top Pipes and bottom Pipes.
+    Random random = new Random();  // for shifting pipes upwards with random values
 
     FlappyBird(){
         this.setPreferredSize(new Dimension(frameWidth, frameHeight));
@@ -81,11 +86,9 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         topPipeImg = new ImageIcon(getClass().getResource("toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("bottompipe.png")).getImage();
 
-        // Construct bird
-        bird = new Bird(flappyBirdImg);
-        // Construction of Pipes list
-        pipesArrayList = new ArrayList<Pipe>();
 
+        bird = new Bird(flappyBirdImg); // Construct bird
+        pipesArrayList = new ArrayList<Pipe>(); // Construction of Pipes list
 
         // Game Loop for repeating of paintComponent() method.
         gameLoop = new Timer(1000/60, this);
@@ -115,19 +118,41 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     public void keyReleased(KeyEvent e) {}
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) { // This action-performed method id for gameLoop
         move();
         repaint();
+
+        if (gameOver){
+            placePipesTimer.stop();
+            gameLoop.stop();
+        }
+    }
+
+    // Collision of Bird only if the bird is collided with pipes.
+    public boolean collision(Bird a, Pipe b){
+        return a.x < b.x + b.width &&  // a's top left corner doesn't reach to b's top right corner
+                a.x + a.width > b.x &&   // a's top right corner passes b's top left corner.
+                a.y < b.y + b.height &&  // a's top left corner doesn't reach b's bottom left corner.
+                a.y + a.height > b.y; // a's bottom left corner passes b's top left corner.
     }
 
     public void placePipes(){
         // (0 - 1) * pipeHeight/2 --> (0 - 256)
         // 0 - 128 - (0 - 256) ---> 1/4 pipeHeight ---> 3/4 pipeHeight
-
         int randomPipeY = (int) (pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2));
+
+        // Creating space for passing the bird between the pipes. Also for shifting bottom pipes.
+        int passingSpace = frameHeight/4;
+
+        // Top Pipe
         Pipe topPipe = new Pipe(topPipeImg);
-        topPipe.y = randomPipeY;
+        topPipe.y = randomPipeY; // Top Pipe starts with random shift from upwards
         pipesArrayList.add(topPipe);
+
+        // Bottom Pipe
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + topPipe.height + passingSpace; // From where bottom pipe starts
+        pipesArrayList.add(bottomPipe);
     }
 
     @Override
@@ -140,9 +165,11 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     public void draw(Graphics g){
         // drawing the background of the game.
         g.drawImage(gameBackground, 0, 0, frameWidth, frameHeight, null);
+
         // drawing the bird
         g.drawImage(bird.birdImg, bird.x, bird.y, bird.width, bird.height, null);
 
+        //For draw pipes in the frame from ArrayList<Pipe>
         for(int i = 0; i<pipesArrayList.size(); i++){
             Pipe pipe = pipesArrayList.get(i);
             g.drawImage(pipe.pipeImg, pipe.x, pipe.y, pipe.width, pipe.height, null);
@@ -162,9 +189,17 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         for (int i = 0; i<pipesArrayList.size(); i++){
             Pipe pipe = pipesArrayList.get(i);
             pipe.x += velocityX;
+
+            if(collision(bird, pipe))
+            {
+                gameOver = true;
+            }
         }
 
-
+        // Bird is fallen out of the frame then game is over.
+        if(bird.y > frameHeight){
+            gameOver = true;
+        }
     }
 
 }
